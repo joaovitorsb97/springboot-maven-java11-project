5ff2a15fac9120e829c0bb642272844f81fed33e
joaovitorsb97 <jvs.bastos@gmail.com>
2022-07-28 17:29:53 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/joaovitor/course/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.joaovitor.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.joaovitor.course.entities.Costumer;
+import com.joaovitor.course.repositories.ConstumerRepository;
+
+@Profile("test") //same name from application.properties spring.profile.active
+@Configuration
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private ConstumerRepository costumerRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Costumer u1 = new Costumer(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		Costumer u2 = new Costumer(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		costumerRepository.saveAll(Arrays.asList(u1, u2));
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/joaovitor/course/repositories/ConstumerRepository.java
@@ -0,0 +1,9 @@
+package com.joaovitor.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaovitor.course.entities.Costumer;
+
+public interface ConstumerRepository extends JpaRepository<Costumer, Long>{
+
+}
