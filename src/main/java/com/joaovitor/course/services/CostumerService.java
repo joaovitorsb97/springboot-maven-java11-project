009484bb08cf5ebbd70d264089c6e70df88d2c5e
joaovitorsb97 <jvs.bastos@gmail.com>
2022-08-10 20:38:49 -0300

    Exception Handler - delete

--- src/main/java/com/joaovitor/course/services/CostumerService.java
+++ src/main/java/com/joaovitor/course/services/CostumerService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.joaovitor.course.entities.Costumer;
 import com.joaovitor.course.repositories.CostumerRepository;
+import com.joaovitor.course.services.exceptions.DatabaseException;
 import com.joaovitor.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class CostumerService {
 	}
 	
 	public void delete(Long id) {
-		costumerRepository.deleteById(id);
+		try {
+			costumerRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Costumer update(Long id, Costumer costumer) {
